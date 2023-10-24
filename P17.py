b4743241bdbaf22334d48653834bcc9ce1a9d9ab
Mirza Hamza Nadeem <98513834+MIRZA-HAMZA341@users.noreply.github.com>
2023-10-24 20:50:03 +0500

    Add files via upload

---
+++ P17.py
@@ -0,0 +1,7 @@
+#While Loop
+i = 1
+while i < 8:
+    print(i)
+    i += 2
+else:
+        print("i is no longer less than 8")
---
+++ P18.py
@@ -0,0 +1,6 @@
+#For Loop
+cars = ["merci", "beemer", "chevy", "skoda"]
+for x in cars:
+  print(x)
+  if x == "chevy":
+    break
